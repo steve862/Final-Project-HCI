fa1c41c4d3a05995812d4945d48b79f73ec4be99
steve862 <87558290+steve862@users.noreply.github.com>
2026-05-25 09:41:22 +0800

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,12 @@
+/* global bootstrap: false */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+function sidebarCollapse() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("collapsed");    
+}
